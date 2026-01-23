badfb8f5ad1efa456f05e101793a56facc18213b
Dmitriy Terletskiy <daterletskiy@gmail.com>
2026-01-23 13:48:20 +0200

    trace macros extention
    Implement possibilitu to have trace log macros with empty parameters^
    ```
    CARPC_TRACE_LOGTRACE( );
    ```
    In this case only metadata information will be traced like file, line,
    function, timestamp, thread an etc.
    
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- api/carpc/trace/macros.h
+++ api/carpc/trace/macros.h
@@ -11,46 +11,46 @@
    #define CARPC_TRACE_SCOPE( msg ) \
       carpc::trace::Scope _carpc_trace_scope_##__LINE__( msg )
 
-   #define CARPC_TRACE_LOG_TRACE( fmt, ... )    \
-      carpc::trace::Log::log(                   \
-            carpc::trace::eLevel::Trace,        \
-            __FILE__, __LINE__, __FUNCTION__,   \
-            fmt, ##__VA_ARGS__                  \
+   #define CARPC_TRACE_LOG_TRACE( fmt, ... )       \
+      carpc::trace::Log::log(                      \
+            carpc::trace::eLevel::Trace,           \
+            __FILE__, __LINE__, __FUNCTION__,      \
+            "" fmt, ##__VA_ARGS__                  \
          )
 
-   #define CARPC_TRACE_LOG_DEBUG( fmt, ... )    \
-      carpc::trace::Log::log(                   \
-            carpc::trace::eLevel::Debug,        \
-            __FILE__, __LINE__, __FUNCTION__,   \
-            fmt, ##__VA_ARGS__                  \
+   #define CARPC_TRACE_LOG_DEBUG( fmt, ... )       \
+      carpc::trace::Log::log(                      \
+            carpc::trace::eLevel::Debug,           \
+            __FILE__, __LINE__, __FUNCTION__,      \
+            "" fmt, ##__VA_ARGS__                  \
          )
 
-   #define CARPC_TRACE_LOG_INFO( fmt, ... )     \
-      carpc::trace::Log::log(                   \
-            carpc::trace::eLevel::Info,         \
-            __FILE__, __LINE__, __FUNCTION__,   \
-            fmt, ##__VA_ARGS__                  \
+   #define CARPC_TRACE_LOG_INFO( fmt, ... )        \
+      carpc::trace::Log::log(                      \
+            carpc::trace::eLevel::Info,            \
+            __FILE__, __LINE__, __FUNCTION__,      \
+            "" fmt, ##__VA_ARGS__                  \
          )
 
-   #define CARPC_TRACE_LOG_WARNING( fmt, ... )  \
-      carpc::trace::Log::log(                   \
-            carpc::trace::eLevel::Warn,         \
-            __FILE__, __LINE__, __FUNCTION__,   \
-            fmt, ##__VA_ARGS__                  \
+   #define CARPC_TRACE_LOG_WARNING( fmt, ... )     \
+      carpc::trace::Log::log(                      \
+            carpc::trace::eLevel::Warn,            \
+            __FILE__, __LINE__, __FUNCTION__,      \
+            "" fmt, ##__VA_ARGS__                  \
          )
 
-   #define CARPC_TRACE_LOG_ERROR( fmt, ... )    \
-      carpc::trace::Log::log(                   \
-            carpc::trace::eLevel::Error,        \
-            __FILE__, __LINE__, __FUNCTION__,   \
-            fmt, ##__VA_ARGS__                  \
+   #define CARPC_TRACE_LOG_ERROR( fmt, ... )       \
+      carpc::trace::Log::log(                      \
+            carpc::trace::eLevel::Error,           \
+            __FILE__, __LINE__, __FUNCTION__,      \
+            "" fmt, ##__VA_ARGS__                  \
          )
 
-   #define CARPC_TRACE_LOG_CRITICAL( fmt, ... ) \
-      carpc::trace::Log::log(                   \
-            carpc::trace::eLevel::Critical,     \
-            __FILE__, __LINE__, __FUNCTION__,   \
-            fmt, ##__VA_ARGS__                  \
+   #define CARPC_TRACE_LOG_CRITICAL( fmt, ... )    \
+      carpc::trace::Log::log(                      \
+            carpc::trace::eLevel::Critical,        \
+            __FILE__, __LINE__, __FUNCTION__,      \
+            "" fmt, ##__VA_ARGS__                  \
          )
 
 #else
